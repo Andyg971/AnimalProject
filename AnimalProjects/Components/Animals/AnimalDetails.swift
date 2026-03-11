5b056fcd937e8c27ac377fa8dab993ec7a270dc7
Avistew <avistew@gmail.com>
2026-03-11 11:28:24 +0100

    changé écran détail de l'animal pour que les données soient prisent d'un tableau (pour l'instant test stocké dans AnimalModel)

--- AnimalProjects/Components/Animals/AnimalDetails.swift
+++ AnimalProjects/Components/Animals/AnimalDetails.swift
@@ -13,66 +13,47 @@ struct AnimalDetails: View {
         ZStack {
             Color.grisFond
                 .ignoresSafeArea()
-            
+
             ScrollView(showsIndicators: false) {
                 VStack {
-                    
+
                     Image(systemName: "questionmark")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 80, height: 80)
                         .padding(16)
-                    
+
                     Text("ID : \(animal.id)")
                         .font(.system(size: 20, weight: .bold))
                     Text("\(animal.species.rawValue) \(animal.race)")
                         .font(.system(size: 16))
-                    
+
                     HStack {
                         Text("POIDS")
                         Text("STATUT")
                         Text("AGE")
                     }
-                    
-                    DetailCard {
-                        
-                        SectionHeader(
-                            icon: "cross.case.fill",
-                            color: .red,
-                            title: "Santé"
-                        )
-                        
-                        InfoRow(label: "Vaccins", value: "À jour")
-                        InfoRow(label: "Dernier Examen", value: "21/02/2026")
-                        InfoRow(label: "Alertes", value: "Aucune")
-                    }
-                    
-                    DetailCard {
-                        
-                        SectionHeader(
-                            icon: "microbe.fill",
-                            color: .vertAccent,
-                            title: "Reproduction"
-                        )
-                        
-                        InfoRow(label: "Insémination", value: "-")
-                        InfoRow(label: "Mise bas estimée", value: "-")
-                    }
-                    
-                    DetailCard {
-                        
-                        SectionHeader(
-                            icon: "chart.bar.xaxis",
-                            color: .blue,
-                            title: "Production"
-                        )
-                        
-                        InfoRow(
-                            label: "Titre d'une info",
-                            value: "Détail de l'info"
-                        )
-                    }
-                    
+
+                    DetailCard(
+                        icon: "cross.case.fill",
+                        color: .red,
+                        title: "Santé",
+                        infoRows: healthRows
+                    )
+
+                    DetailCard(
+                        icon: "microbe.fill",
+                        color: .vertAccent,
+                        title: "Reproduction",
+                        infoRows: reproductionRows
+                    )
+
+                    DetailCard(
+                        icon: "chart.bar.xaxis",
+                        color: .blue,
+                        title: "Production",
+                        infoRows: productionRows
+                    )
                 }
                 .navigationTitle("Détails de l'animal")
                 .toolbarBackground(
@@ -83,9 +64,9 @@ struct AnimalDetails: View {
                 .toolbarBackground(.visible, for: .navigationBar)
             }
         }
-        
+
     }
-    
+
 }
 
 #Preview {
