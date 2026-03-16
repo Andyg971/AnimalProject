0a411b0354e76991444d00ce9da4bbedef270f8a
Avistew <avistew@gmail.com>
2026-03-16 17:38:38 +0100

    Ajouté écran basique pour Production

---
+++ AnimalProjects/Data/Models/ProductionModel.swift
@@ -0,0 +1,186 @@
+//
+//  ProductionModel.swift
+//  AnimalProjects
+//
+//  Created by apprenant79 on 16/03/2026.
+//
+
+import Observation
+import SwiftUI
+
+struct ProductionResponse: Codable {
+    let records: [ProductionRecord]
+}
+
+struct ProductionRecord: Codable {
+    let fields: ProductionField
+}
+
+struct ProductionField: Codable, Identifiable {
+    var id: Int
+    let date: Date
+    let productionType: ProductionType
+    let amount: Double
+    let unit: String
+    let tB: Double?
+    let tP: Double?
+    let cellCount: Int?
+    let ketone: Int?
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "productionID"
+        case date
+        case productionType
+        case amount
+        case unit
+        case tB = "TB"
+        case tP = "TP"
+        case cellCount
+        case ketone
+    }
+
+    init(
+        id: Int,
+        date: Date,
+        productionType: ProductionType,
+        amount: Double,
+        unit: String,
+        tB: Double? = nil,
+        tP: Double? = nil,
+        cellCount: Int? = nil,
+        ketone: Int? = nil
+    ) {
+        self.id = id
+        self.date = date
+        self.productionType = productionType
+        self.amount = amount
+        self.unit = unit
+        self.tB = tB
+        self.tP = tP
+        self.cellCount = cellCount
+        self.ketone = ketone
+    }
+
+}
+
+class ProductionData: Identifiable {
+    var id: Int
+    let date: Date
+    let productionType: ProductionType
+    let amount: Double
+    let unit: String
+
+    init(
+        id: Int,
+        date: Date,
+        productionType: ProductionType,
+        amount: Double,
+        unit: String
+    ) {
+        self.id = id
+        self.date = date
+        self.productionType = productionType
+        self.amount = amount
+        self.unit = unit
+    }
+}
+
+class ProductionMilk: ProductionData {
+    let tB: Double
+    let tP: Double
+    let cellCount: Int
+    let ketone: Int?
+
+    init(
+        id: Int,
+        date: Date,
+        productionType: ProductionType,
+        amount: Double,
+        unit: String,
+        tB: Double,
+        tP: Double,
+        cellCount: Int,
+        ketone: Int? = nil
+    ) {
+        self.tB = tB
+        self.tP = tP
+        self.cellCount = cellCount
+        self.ketone = ketone
+        super.init(
+            id: id,
+            date: date,
+            productionType: productionType,
+            amount: amount,
+            unit: unit
+        )
+    }
+}
+
+enum ProductionType: String, Codable, CaseIterable, Identifiable {
+    var id: RawValue { rawValue }
+    case milk = "Lait"
+    case wool = "Laine"
+    case meat = "Viande"
+
+    var symbol: String {
+        switch self {
+        case .milk:
+            "drop.fill"
+        case .meat:
+            "fork.knife"
+        case .wool:
+            "line.3.crossed.swirl.circle.fill"
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .milk:
+            .white
+        case .meat:
+            .red
+        case .wool:
+            .gray
+        }
+    }
+}
+
+func convertProduction(_ record: ProductionRecord) -> ProductionData {
+    let fields = record.fields
+    if fields.productionType == .milk {
+        return ProductionMilk(
+            id: record.fields.id,
+            date: record.fields.date,
+            productionType: .milk,
+            amount: Double(record.fields.amount),
+            unit: "L",
+            tB: fields.tB!,
+            tP: fields.tP!,
+            cellCount: fields.cellCount!,
+            ketone: fields.ketone
+        )
+    } else {
+        return ProductionData(
+            id: fields.id,
+            date: fields.date,
+            productionType: fields.productionType,
+            amount: fields.amount,
+            unit: fields.unit
+        )
+    }
+}
+
+func weeklyProduction(from data: [ProductionData]) -> [(
+    week: Date, amount: Double
+)] {
+    let calendar = Calendar.current
+
+    let grouped = Dictionary(grouping: data) { prod in
+        calendar.dateInterval(of: .weekOfYear, for: prod.date)!.start
+    }
+
+    return grouped.map { (week, values) in
+        (week: week, amount: values.reduce(0) { $0 + $1.amount })
+    }
+    .sorted { $0.week < $1.week }
+}
