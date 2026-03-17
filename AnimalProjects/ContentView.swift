744b29178a172f49e8b524e1aa94b23c3df35283
Julia Canovas <e3asy@MacBook-Air-de-Julia.local>
2026-03-17 16:11:06 +0100

    ajout contentview, supp maintabview

--- AnimalProjects/ContentView.swift
+++ AnimalProjects/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+    
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "grisFond")
+        
+        appearance.stackedLayoutAppearance.normal.iconColor = .gray
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(named:"vertAccent")
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor(named: "vertAccent")!]
+        
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
     var body: some View {
-        VStack {
+   
+        TabView {
             
-        }
-        .padding()
+            DashboardView()
+                .tabItem{
+                    Label("Tableau de bord", systemImage: "doc.fill")
+
+                }
+            AnimalView()
+                .tabItem{
+                    Label("Mes Animaux", systemImage: "pawprint.fill")
+                }
+            CommunityView()
+                .tabItem{
+                    Label("Communauté", systemImage: "message.fill")
+                }
+            UserProfileView(employee: Employee(
+                id: 9,
+                firstName: "Thomas",
+                lastName: "Faure",
+                photo: [],
+                position: "Conducteur d'élevage",
+                zone: [],
+                userName: "thomasf",
+                email: "thomas.faure@domaine-s.fr",
+                exploitation: ["recqRtgdpGSlWGC5B"],
+                city: "Sainte-Foy-lès-Lyon",
+                recordID: "recqRtgdpGSlWGC5B"
+            ))
+                .tabItem{
+                    Label("Profil", systemImage: "person.circle")
+                }
+        }.accentColor(.vertAccent)
+            .tabViewStyle(.automatic)
     }
 }
-
 #Preview {
     ContentView()
 }
